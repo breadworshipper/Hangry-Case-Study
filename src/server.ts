3dc82015d8236c20aec481beaf3abe3caa7fe21f
breadworshipper <azhrayashna@gmail.com>
2024-08-12 19:13:46 +0700

    FEAT: POST User

--- src/server.ts
+++ src/server.ts
@@ -12,13 +12,59 @@ let db = new sqlite3.Database(
   }
 )
 
-const PORT: number = Number(process.env.PORT);
+const PORT: number = 3000
 
 const server: http.Server = http.createServer(
     (req: http.IncomingMessage, res: http.ServerResponse) => {
 
     if (res.headersSent) {
       return
+    }
+
+    if (req.url === "/user" && req.method === "POST") {
+      // Read body of the request
+        let body: string = ""
+        req.on("data", (chunk: string) => {
+          body += chunk
+        })
+
+        req.on("end", () => {
+          try {
+            const parsedBody = JSON.parse(body)
+
+            // Validate required attributes
+            if (
+              !parsedBody.name ||
+              !parsedBody.email ||
+              !parsedBody.date_of_birth
+            ) {
+              res.statusCode = 400
+              res.setHeader("Content-Type", "application/json")
+              res.end(
+                JSON.stringify({ message: "Missing required attributes" })
+              )
+              return
+            }
+
+            db.run(
+              `INSERT INTO users (name, email, date_of_birth) VALUES (?, ?, ?)`,
+              [parsedBody.name, parsedBody.email, parsedBody.date_of_birth],
+              (err) => {
+                if (err) {
+                  console.error("Failed to insert user:", err)
+                } else {
+                  res.statusCode = 201
+                  res.setHeader("Content-Type", "application/json")
+                  res.end(JSON.stringify({ message: "User created" }))
+                }
+              }
+            )
+          } 
+          catch (error) {
+            console.error("Failed to parse JSON:", error)
+          }
+        }
+      )   
     }
   }
 )
